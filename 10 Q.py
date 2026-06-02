8bfb4157143126f0a93f7b407d9b6a7a75cb2af6
SwayamPrakash2007 <swayamsbp@gmail.com>
2026-06-02 14:42:08 +0530

    Add files via upload

---
+++ 10 Q.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Create 2D array
+a = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+# Print shape
+print("Shape:", a.shape)
+
+# Print size
+print("Size:", a.size)
+
+# Print itemsize
+print("Item Size:", a.itemsize)
+
+# Print dtype
+print("Datatype:", a.dtype)
+
+# Change datatype
+b = a.astype(float)
+
+print("Changed Datatype:", b.dtype)
